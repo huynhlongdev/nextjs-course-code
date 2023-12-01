ed8e9c8ed8eea17b4c9b5ce374915835b3529018
thai long <thailongqsqn@gmail.com>
2023-12-01 15:44:45 +0700

    tesst

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,17 +1,18 @@
-import Head from 'next/head';
-import { Fragment } from 'react';
+import Head from "next/head";
+import { Fragment } from "react";
 
-import AllPosts from '../../components/posts/all-posts';
-import { getAllPosts } from '../../lib/posts-util';
+import AllPosts from "../../components/posts/all-posts";
+import { getAllPosts } from "../../lib/posts-util";
 
 function AllPostsPage(props) {
+  console.log(props);
   return (
     <Fragment>
       <Head>
         <title>All Posts</title>
         <meta
-          name='description'
-          content='A list of all programming-related tutorials and posts!'
+          name="description"
+          content="A list of all programming-related tutorials and posts!"
         />
       </Head>
       <AllPosts posts={props.posts} />
